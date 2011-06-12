3837e5262d3d1c50e5d21c2129acec10523f9091
Christoph Schroeder <nutri.matic@yahoo.de>
2011-06-12 17:34:55 +0200

    Roughly adapted the template app to the modified libraries. Still work with event handling at keys and mouse.

--- projects/template/myApp.js
+++ projects/template/myApp.js
@@ -1,4 +1,28 @@
-
+import("myEvents.js");
+import("myNodes.js");
+import("myScene.js");
 
 function MyApp(){};
 MyApp.prototype = new App;
+
+/**
+ * Override getShader
+ */
+MyApp.prototype.getShader = function() {
+	return new Shader().init(this.gl, "../../shader/simple.vertex", "../../shader/white.fragment");
+};
+
+/**
+ * Override MyScene
+ */
+MyApp.prototype.getScene = function() {
+	return new MyScene().init(this.gl, this.canvas, this.aspectRatio, this.shader);
+};
+
+/**
+ * Override getEventManager
+ */
+MyApp.prototype.getEventManager = function() {
+	return new MyEventManager().init(this);
+};
+
--- projects/template/myEvents.js
+++ projects/template/myEvents.js
@@ -1,14 +1,10 @@
-
-
-function MyEventManager(){
-};
+function MyEventManager(){};
 MyEventManager.prototype = new EventManager;
 
 MyEventManager.prototype.handleKeyEvents = function() {
 	with (this) {
 	}
 };
-
 
 MyEventManager.prototype.handleMouseEvents = function() {
 	with (this) {
