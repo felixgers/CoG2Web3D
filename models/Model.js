68953c4a6a8b1fc0e376eaf7e700dd6c66013041
Christoph Schroeder <nutri.matic@yahoo.de>
2011-06-12 17:32:18 +0200

    Had to modify loading in order to not get any browser errors, because with no overridemimetype to "text/plain" the HTTTPRequest loading would try to interpret the shader code as javascript and with request.status 200 the loading would fail. This might be an effect, which appears, if one uses the framework on the local filesystem.
    Added some semicola to avoid eclipse javascript parser warnings.

--- models/Model.js
+++ models/Model.js
@@ -1,116 +1,113 @@
 function Model(filename,gl,shader){
-  this.vertices;
-  this.colors;
-  this.vertexPositionBuffer;
-  this.vertexNormalBuffer;
-  this.vertexTextureCoordBuffer=null;
-  this.vertexIndexBuffer;
-  this.texture;
-  this.colorBuffer;
-  this.gl=gl;
-  this.loaded=false;   
- 
-  
-   $.ajaxSetup({'beforeSend': function(xhr){
+	this.vertices;
+	this.colors;
+	this.vertexPositionBuffer;
+	this.vertexNormalBuffer;
+	this.vertexTextureCoordBuffer=null;
+	this.vertexIndexBuffer;
+	this.texture;
+	this.colorBuffer;
+	this.gl=gl;
+	this.loaded=false;   
+
+
+	$.ajaxSetup({'beforeSend': function(xhr){
 		if (xhr.overrideMimeType)
 			xhr.overrideMimeType("text/json");
-		}
+	}
 	}); 		
-  
-   Model.prototype.setTexture=function(texture){
-   		this.texture=texture;
-   }
-   
-   Model.prototype.init=function(gl, pMatrix, mvMatrix, shaderProgram){
+
+	Model.prototype.setTexture=function(texture){
+		this.texture=texture;
+	};
+
+	Model.prototype.init=function(gl, pMatrix, mvMatrix, shaderProgram){
 		this.gl=gl;
 		this.shaderProgram=shaderProgram;
 		this.pMatrix=pMatrix;
 		this.mvMatrix=mvMatrix;
-   }
+	};
 
-   Model.prototype.loadData=function(){
-			//is json loaded
-			if(this.data==null)return;
-			var data=this.data; 
-			var gl=this.gl;
-			if(data.cube.t != null){
-				this.vertexTextureCoordBuffer = gl.createBuffer();
-				gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
-				gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.cube.t), gl.STATIC_DRAW);
-				this.vertexTextureCoordBuffer.itemSize = 2;
-				this.vertexTextureCoordBuffer.numItems = data.cube.t.length / 2;
-			}
-			if(data.cube.m != null){
-				this.colorBuffer = gl.createBuffer();
-				gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
-				gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.cube.m), gl.STATIC_DRAW);
-				this.colorBuffer.itemSize = 4;
-				this.colorBuffer.numItems = data.cube.m.length / 2;
-			}			
-		
-			this.vertexPositionBuffer = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.cube.v), gl.STATIC_DRAW);
-			this.vertexPositionBuffer.itemSize = 3;
-			this.vertexPositionBuffer.numItems = data.cube.v.length / 3;
-		
-			this.vertexIndexBuffer = gl.createBuffer();
-			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
-			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data.cube.i), gl.STATIC_DRAW);
-			this.vertexIndexBuffer.itemSize = 1;
-			this.vertexIndexBuffer.numItems = data.cube.i.length;
-
-			this.loaded=true;
-					
-   }
-   
-    Model.prototype.draw = function(){			
-		with(this){	
-				if(loaded){
-					gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-					gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-					
-					if(vertexTextureCoordBuffer != null){
-						//todo texture
-					}else if(colorBuffer != null){
-						gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-						gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, colorBuffer.itemSize, gl.FLOAT, false, 0, 0);
-					}
-				  
-					gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
-					gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-					gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-					gl.drawElements(gl.TRIANGLES,vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-				}	
-			}	
-
+	Model.prototype.loadData=function(){
+		//is json loaded
+		if(this.data==null)return;
+		var data=this.data; 
+		var gl=this.gl;
+		if(data.cube.t != null){
+			this.vertexTextureCoordBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.cube.t), gl.STATIC_DRAW);
+			this.vertexTextureCoordBuffer.itemSize = 2;
+			this.vertexTextureCoordBuffer.numItems = data.cube.t.length / 2;
 		}
-   
-   
-   
-   
+		if(data.cube.m != null){
+			this.colorBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.cube.m), gl.STATIC_DRAW);
+			this.colorBuffer.itemSize = 4;
+			this.colorBuffer.numItems = data.cube.m.length / 2;
+		}			
+
+		this.vertexPositionBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.cube.v), gl.STATIC_DRAW);
+		this.vertexPositionBuffer.itemSize = 3;
+		this.vertexPositionBuffer.numItems = data.cube.v.length / 3;
+
+		this.vertexIndexBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data.cube.i), gl.STATIC_DRAW);
+		this.vertexIndexBuffer.itemSize = 1;
+		this.vertexIndexBuffer.numItems = data.cube.i.length;
+
+		this.loaded=true;
+	};
+
+	Model.prototype.draw = function(){			
+		with(this){	
+			if(loaded){
+				gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+				gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+				if(vertexTextureCoordBuffer != null){
+					//todo texture
+				}else if(colorBuffer != null){
+					gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+					gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, colorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+				}
+
+				gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+				gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+				gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+				gl.drawElements(gl.TRIANGLES,vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+			}	
+		}	
+
+	};
+
+
 	Model.prototype.load=function(url,model) {
-		 var that=model;
-		
-		
+		var that=model;
+
 		try { var request = new XMLHttpRequest(); }
 		catch(e) { 
 			alert("Could not create XMLHttpRequest"); 
 			return null;
 		}
 		// Do not open asynchronously, thus wait for the response.
+		request.overrideMimeType("text/plain");
 		request.open("GET", url, false); 
 		request.send(null);
 		// Check if we got HTTP status 200 (OK)
-		if (request.status == 200) {
+		if (request.status == 0) {
 			that.data=JSON.parse(request.responseText);
 			that.loadData();
 		} else { // Failed
-			alert("Could not load shader file: "+url); 
+			alert("Could not load model file: "+url); 
 			return null;
 		}
 	};
-   
+
 	/*
 	Model.prototype.load=function(filename,model){
 	     var that=model;
@@ -124,9 +121,9 @@ function Model(filename,gl,shader){
 	 .error(function() { alert("error"); })
 	 .complete(function() { alert("complete"); });;
 	}
-	*/
-	
+	 */
+
 	this.load(filename,this);
-	
+
 }
 //Model.prototype=new Shape(); 
