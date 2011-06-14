a5a42a56cb64b2f18963433a7635d61691d3d7fd
Felix Gers <gers@beuth-hochschule.de>
2011-06-14 18:04:08 +0200

    working app and scene

--- projects/template/myEvents.js
+++ projects/template/myEvents.js
@@ -1,6 +1,18 @@
 
+function MyEventManager(){
+};
+MyEventManager.prototype = new EventManager;
+
+MyEventManager.prototype.handleKeyEvents = function() {
+	with (this) {
+	}
+};
 
 
+MyEventManager.prototype.handleMouseEvents = function() {
+	with (this) {
+	}
+};
 
 
 /**
