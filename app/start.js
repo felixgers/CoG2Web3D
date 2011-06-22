0a820a831ffed6dd465f8d668ae80d7b28ccfc72
Andrea Kuschan <Andrea@.(none)>
2011-06-22 21:37:13 +0200

    change start.js

--- app/start.js
+++ app/start.js
@@ -1,3 +1,27 @@
+/**
+ * Import javascripts
+ * @param javascriptPath optinalHTMLelement
+ */
+
+function import(javascriptPath) {
+    try { var request = new XMLHttpRequest(); }
+    catch(e) { alert("Could not create XMLHttpRequest");  return; }
+    var localFileSys = document.URL.match(/^file:\/\/.*?$/);
+    request.open("GET", javascriptPath, false);
+    request.overrideMimeType("text/plain");
+    request.send(null);
+    if ((localFileSys && request.status == 0) ||
+        request.status == 200) {
+        var script = document.createElement("script");
+        script.setAttribute("type", "text/javascript");
+        var code =document.createTextNode(request.responseText);
+        script.appendChild(code);
+        document.getElementsByTagName("head")[0].appendChild(script);
+    } else { alert("Could not load javascript file: "+javascriptPath);
+return; }    
+}
+
+
 // First import all required JS modules
 // Therefore the HTML document must load the script star.js first.
 // More import statements may follow at the end of JS files to cover dependencies.
@@ -13,14 +37,7 @@ import("../../app/app.js");
 
 
 
-/**
- * Import javascripts
- * @param javascriptPath optinalHTMLelement
- */
-function import(javascriptPath) {
-	var script = document.createElement("script");
-	script.setAttribute("type", "text/javascript");
-	script.setAttribute("src", javascriptPath);
-	var absPath = script.src;
-	document.getElementsByTagName("head")[0].appendChild(script);
-}
+window.onload = function () {
+	new MyApp().start('canvas');
+};
+
--- projects/2011_SS_andrea/myApp.js
+++ projects/2011_SS_andrea/myApp.js
@@ -1,23 +1,21 @@
-import("../../models/Model.js");
-import("../../util/jquery.js");
-import("json2.js");
-
 import("myEvents.js");
 import("myNodes.js");
 import("myScene.js");
 
-
 function MyApp(){};
-MyApp.prototype = new App();
+MyApp.prototype = new App;
 
+/**
+ * Override Shader
+ */
 MyApp.prototype.createShader = function() {
 	return new Shader().init(this.gl, "../../shader/color.vertex", "../../shader/color.fragment");
 };
 
+
 /**
- * Override MyScene
+ * Override Scene
  */
 MyApp.prototype.createScene = function() {
 	return new MyScene().init(this.gl, this.canvas, this.aspectRatio, this.shader);
 };
-
