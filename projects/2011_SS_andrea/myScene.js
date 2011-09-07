8272872cb277903004a1273a221a6cdfd6cbecfd
aku1 <kuschan@arcor.de>
2011-09-07 17:05:58 +0200

    change project structure

--- projects/2011_SS_andrea/myScene.js
+++ projects/2011_SS_andrea/myScene.js
@@ -1,5 +1,5 @@
 importScript("../../models/Model.js");
-importScript("../../util/jquery.js");
+importScript("../../ext/jquery.js");
 importScript("json2.js");
 
 function MyScene(){}
@@ -13,43 +13,21 @@ MyScene.prototype.buildSceneGraph = function() {
 		var sceneGraph = new Group();
 		var camera = new PerspectiveCamera(this.verticalViewAngle, this.aspectRatio , 1, 1000);
 
-		// Create 1st group with triangle.
-		/*
-		var g2 = new Group();
-		g2.addChild(new Translation(1.5, 1.5, -8.0));
-		g2.addChild(new RotorY(0.5));
-		var cubeYellow=new Model('cubeYellow.json',this.gl,this.shader);
-		g2.addChild(cubeYellow);
-
-	
-		
-		// Create 3rd group with box.
-		var g3 = new Group();
-		g3.addChild(new Translation(0, 0, -12));
-		g3.addChild(new RotorY(0.5));
-		
-	
-		var cube=new Model('jumpingBall.json',this.gl,this.shader);
-		if(cube.hasAnimations){
-		    cube.animation.setMatrices(this.matrices);
-			g3.addChild(cube.animation);
-		}
-		g3.addChild(cube);
-		*/
-		
-			// Create 2st group with rectangle.
+		// Create 2st group with rectangle.
 		var g1 = new Group();
 		g1.addChild(new Translation(-6, -5, -20.0));
 		//g1.addChild(new RotorY(0.5));
 		g1.addChild(new Scale(0.03469776,0.05087215,0.6585168));
 		  
 		  
-		var monkey=new Model('test.json',this.gl,this.shader);
+		var monkey=new Model(this.gl,this.shader);
+        monkey.loadJsonFile('test.json');
 		g1.addChild(monkey);
 		
 		var g4 = new Group();
 		g4.addChild(new Translation(-6, -5, -80.0));
-		var table=new Model('table.json',this.gl,this.shader);
+		var table=new Model(this.gl,this.shader);
+        table.loadJsonFile('table.json');
 		g4.addChild(table);
 
 		// Add all nodes to scene graph.
